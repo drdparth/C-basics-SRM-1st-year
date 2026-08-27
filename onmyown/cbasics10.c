722a14cfc41b797a9df71971ed9b2463b2e6dfee
Parth Singh <parthsingh.drd@gmail.com>
2026-08-27 11:36:29 +0530

    Add files via upload

---
+++ onmyown/cbasics10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int m;
+    printf("Input your code motherfucker:\n");
+    scanf("%d" , m);
+
+    if(m >= 15 || m <= 13){
+        printf("You motherfucker get outa here.");
+    }
+    else{
+        printf("Hello dickhead.");
+    }
+    return 0 ;
+}
